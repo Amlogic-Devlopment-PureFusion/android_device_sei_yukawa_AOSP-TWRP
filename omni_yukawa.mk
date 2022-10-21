50a9aa52bd68f11ea3d000c19f16bc52d41e265d
Eliminater74 <eliminater74@gmail.com>
2022-10-20 22:06:05 -0400

    yukawa: Initial TWRP device tree
    Made with SebaUbuntu's TWRP device tree generator
    Arch: arm64
    Manufacturer: sei
    Model: ATV on yukawa
    Script version: 3.0.0

---
+++ omni_yukawa.mk
@@ -0,0 +1,29 @@
+#
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Inherit from yukawa device
+$(call inherit-product, device/sei/yukawa/device.mk)
+
+PRODUCT_DEVICE := yukawa
+PRODUCT_NAME := omni_yukawa
+PRODUCT_BRAND := generic
+PRODUCT_MODEL := ATV on yukawa
+PRODUCT_MANUFACTURER := sei
+
+PRODUCT_GMS_CLIENTID_BASE := android-sei
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="yukawa-userdebug 12 SP2A.220505.008 eng.elimin.20221019.191811 test-keys"
+
+BUILD_FINGERPRINT := generic/yukawa/yukawa:12/SP2A.220505.008/eliminater7410191856:userdebug/test-keys
