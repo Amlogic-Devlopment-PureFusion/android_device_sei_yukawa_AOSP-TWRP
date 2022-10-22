1236cd19e36053b1dc28b47f92fc2e217c1288d8
Eliminater74 <eliminater74@gmail.com>
2022-10-22 09:52:52 -0400

    BoardConfig: Could use some tweaking

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -33,7 +33,7 @@ TARGET_BOOTLOADER_BOARD_NAME := vim3
 TARGET_NO_BOOTLOADER := true
 
 # Display
-TARGET_SCREEN_DENSITY := 320
+#TARGET_SCREEN_DENSITY := 320
 
 # Kernel
 BOARD_BOOTIMG_HEADER_VERSION := 2
@@ -77,7 +77,7 @@ BOARD_SEI_DYNAMIC_PARTITIONS_SIZE := 9122611200 # TODO: Fix hardcoded value
 TARGET_BOARD_PLATFORM := yukawa
 
 # Recovery
-TARGET_RECOVERY_PIXEL_FORMAT := RGBX_8888
+#TARGET_RECOVERY_PIXEL_FORMAT := RGBX_8888
 TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_USE_F2FS := true
 
@@ -87,8 +87,84 @@ VENDOR_SECURITY_PATCH := 2099-12-31
 PLATFORM_VERSION := 16.1.0
 
 # TWRP Configuration
-TW_THEME := portrait_hdpi
-TW_EXTRA_LANGUAGES := true
-TW_SCREEN_BLANK_ON_BOOT := true
-TW_INPUT_BLACKLIST := "hbtp_vm"
+
+# Misc
+#TW_MTP_DEVICE := /dev/mtp_usb
+#TW_HAS_MTP := true
+#TW_USE_MODEL_HARDWARE_ID_FOR_DEVICE_ID := true
+
+# Debug
+TARGET_USES_LOGD := true
+# include Logcat daemon for help in debugging
+TWRP_INCLUDE_LOGCAT := true
+TWRP_EVENT_LOGGING := false
+
+# TWRP Theme BOARD_SUPER_PARTITION_GROUPS
+TW_THEME := landscape_hdpi
+# Old is gold. Put false to use the old one.
+#TWRP_NEW_THEME := true
+
+# Set the path to the sysfs entry which controls the brightness
+#TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
+
+# Max brightness to prevent display damage
+#TW_MAX_BRIGHTNESS := 255
+
+# Default brightness for TWRP
+#TW_DEFAULT_BRIGHTNESS := 150
+
+# Disable/enable SELinux. Only suggested when you want to enable SELinux support
+#TWHAVE_SELINUX := true
+
+# Set the default language, if not english
+TW_DEFAULT_LANGUAGE := en
+
+# disables things like sdcard partitioning and may save you some space if TWRP isn't fitting in your recovery patition
+#BOARD_HAS_NO_REAL_SDCARD := true
+
+#BOARD_HAS_SDCARD_INTERNAL := true
+
+# TWRP specific build flags
+RECOVERY_VARIANT := twrp
+#RECOVERY_SDCARD_ON_DATA := true
+#TARGET_RECOVERY_PIXEL_FORMAT := ABGR_8888
+#TARGET_RECOVERY_PIXEL_FORMAT := RGBA_8888
+TARGET_RECOVERY_PIXEL_FORMAT := RGBX_8888
+#TARGET_RECOVERY_PIXEL_FORMAT := BGRA_8888
+#TARGET_RECOVERY_FORCE_PIXEL_FORMAT := "RGB_565"
+BOARD_HAS_NO_SELECT_BUTTON := true
+#BOARD_TOUCH_RECOVERY :=
+BOARD_RECOVERY_SWIPE := true
+#BOARD_SUPPRESS_SECURE_ERASE := true
+#TW_USE_NEW_MINADBD := true
 TW_USE_TOOLBOX := true
+
+#TW_INCLUDE_LOGICAL := product odm
+TW_EXTRA_LANGUAGES := false
+TW_SCREEN_BLANK_ON_BOOT := false
+TW_INPUT_BLACKLIST := "hbtp_vm"
+TW_DEVICE_VERSION := $(shell date '+%Y%m%d-%H%M%S') by Eliminater74
+
+TW_OEM_BUILD := true
+#TW_DISABLE_TTF := true
+
+### TW NO'S ###
+TW_NO_SCREEN_TIMEOUT := true
+TW_NO_HAPTICS := true
+TW_NO_LEGACY_PROPS := true
+# Disable the battery percentage for devices where it doesn't work properly
+TW_NO_BATT_PERCENT := true
+# Remove exFAT formatting binaries
+TW_NO_EXFAT := true
+
+# Decryption support for /data
+#TW_INCLUDE_CRYPTO := true
+#TW_INCLUDE_CRYPTO_FBE := true
+#TW_INCLUDE_FBE_METADATA_DECRYPT := false
+#BOARD_USES_METADATA_PARTITION := true
+
+# Tool
+#TW_INCLUDE_REPACKTOOLS := true
+#TW_INCLUDE_RESETPROP := true
+#TW_INCLUDE_LIBRESETPROP :=true
+
